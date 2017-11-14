737145672321a6e684db5ed73abe3d686d5409a3
Hacktiv8 <hacktiv8-6@Hacktiv8-6.local>
2017-11-14 16:19:41 +0700

    Add binary search algorithm and insertion sorting

--- binary.js
+++ binary.js
@@ -4,13 +4,34 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55, 44]
 
 function ownSort(arr) {
-  // Your sorting code
+  var tampungPindahan = 0
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < i; j++) {
+      if (arr[i]<arr[j]) {
+        tampungPindahan = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tampungPindahan;
+      }
+    }
+  }
   return arr
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function binary_search (search, array) {
+  var result = -1;
+  var startIndex = 0;
+  var stopIndex = array.length-1;
+  var midIndex = Math.floor((startIndex+stopIndex)/2);
+  while (search != array[midIndex] && startIndex < stopIndex) {
+    if (array[midIndex]< search) {
+      startIndex = midIndex;
+    }
+    else if (array[midIndex]> search) {
+      stopIndex = midIndex-1;
+    }
+    midIndex = Math.floor((startIndex+stopIndex)/2);
+  }
+  return (array[midIndex]==search)? midIndex : -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
@@ -20,6 +41,7 @@ var arrayGanjilSorted = ownSort(testArrayGanjil)
 console.log(binary_search(8, arrayGenapSorted))
 console.log(binary_search(10, arrayGenapSorted))
 console.log(binary_search(33, arrayGenapSorted))
+console.log(binary_search(22, arrayGenapSorted))
 
 console.log(binary_search(53, arrayGanjilSorted))
 console.log(binary_search(3, arrayGanjilSorted))
