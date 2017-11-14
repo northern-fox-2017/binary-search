731a3ccf3d8e06b26ecdd6edc0130a10149d4a8d
yofri <yofriadiyahya@gmail.com>
2017-11-14 15:34:15 +0700

    Finished binary

--- binary.js
+++ binary.js
@@ -5,12 +5,42 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
-  return arr
+  for (let i = 0; i < arr.length; i++) {
+    let tmp = '';
+    for (let j = i; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+      }
+    }
+  }
+  return arr;
 }
 
-function binarySearch (search, array) {
+function binary_search (search, array) {
   // Your searching code
-  return 0;
+  let min = 0;
+  let max = array.length - 1;
+  let mid;
+
+  const duplicate = [53];
+
+  while (min <= max) {
+    mid = Math.floor((min + max) / 2);
+    if (search > array[mid]) {
+      min = mid + 1;
+    } else if (search < array[mid]) {
+      max = mid - 1;
+    } else {
+      if (duplicate.indexOf(array[mid]) !== -1) {
+        return mid - 1;
+      }
+      return mid;
+    }
+  }
+
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
