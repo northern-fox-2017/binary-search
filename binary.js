092e3eb9c85ff02c065dfe5baac5745c001a91fd
Herman <Home@Herman-MacBook-Pro.local>
2017-11-14 21:49:25 +0700

    heraldoyp

--- binary.js
+++ binary.js
@@ -4,12 +4,37 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55, 44]
 
 function ownSort(arr) {
-  // Your sorting code
-  return arr
+  for(var i=0; i<arr.length; i++){
+    for(var j=i; j<arr.length; j++){
+      if(arr[i] > arr[j]){
+        var temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
 }
 
-function binarySearch (search, array) {
+function binary_search (search, array) {
   // Your searching code
+  var mid = Math.floor(array.length/2);
+  console.log(array);
+  if(!array[mid]){
+    return -1
+  }else{
+     if(array[mid] === search){
+      return mid;
+    }else if(search < array[mid]){
+      return binary_search(search, array.splice(0, mid));
+    }else if(search > array[mid]){
+      if(array[mid] !== search){
+        return -1;
+      }else {
+        return mid + binarySearch(search, array.splice(mid, Math.max.apply(Math, array)));
+      }
+    }
+  }
   return 0;
 }
 
