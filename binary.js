e6def00a048566bca146d1736c31534b7e8fb521
angrha <angrha@gmail.com>
2017-11-14 16:53:32 +0700

    done , aang

--- binary.js
+++ binary.js
@@ -4,13 +4,40 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55, 44]
 
 function ownSort(arr) {
-  // Your sorting code
+  let gelasKosong = 0
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (arr[i] < arr[j]) {
+        gelasKosong = arr[i]
+        arr[i] = arr[j]
+        arr[j] = gelasKosong
+      }
+    }
+  }
+  // console.log(arr);
   return arr
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function binary_search (search, array) {
+  let end = array.length
+  let start = 0
+  let mid = Math.floor((end + start) / 2)
+
+  while(start <= end){
+    if(search == array[mid]){
+      return mid
+    }
+    else if(search < array[mid]){
+      end = mid -1
+      mid = Math.floor((end + start) / 2)
+    }
+    else if(search > array[mid]){
+      start = mid + 1
+      mid = Math.floor((end + start) / 2)
+    }
+  }
+
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
