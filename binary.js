b4a8c6156ff5b6dfaf9495eb8d1e37eef43f5f36
juliusade <juliusade15@gmail.com>
2017-11-14 20:22:10 +0700

    release 1

--- binary.js
+++ binary.js
@@ -5,16 +5,50 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55, 44]
 
 function ownSort(arr) {
   // Your sorting code
+  for(let i=0; i<arr.length; i++){
+    for(let j=0; j<arr.length; j++){
+      if(arr[j] > arr[j+1]) {
+      var sort = arr[j]
+      arr[j] = arr[j+1]
+      arr[j+1] = sort
+      }
+    }
+  }
   return arr
 }
 
-function binarySearch (search, array) {
+function binary_search (search, array) {
   // Your searching code
-  return 0;
+  // console.log(array);
+  let awal = 0;
+  let akhir = array.length-1;
+  let middle = Math.floor((awal + akhir) /2);
+
+  while(search !== array[middle]) {
+    if(search >= array[middle]) {
+      awal = middle+1;
+      akhir = array.length-1;
+      middle = Math.floor((awal + akhir) /2);
+    }
+    if(search <= array[middle]) {
+      awal = 0;
+      akhir = middle -1;
+      middle = Math.floor((awal + akhir) /2);
+    } else {
+      return -1
+    }
+  }
+  return middle;
 }
+
+// console.log(akhir);
+// console.log(middle);
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
+
+// console.log(arrayGenapSorted);
+// console.log(arrayGanjilSorted);
 
 // Driver code
 console.log(binary_search(8, arrayGenapSorted))
