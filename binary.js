3704cf6200ef5653987b0ab54460901b91980008
fabiokounang <fabio_kosukeueki@yahoo.com>
2017-11-14 21:42:55 +0700

    REKURSIF BINARY

--- binary.js
+++ binary.js
@@ -2,7 +2,11 @@
 
 var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55, 44]
-
+var arrayGenapSorted = ownSort(testArrayGenap)
+// // [ 8 , 10, 10, 18, 22, 22, 32, 40, 90 ]
+var arrayGanjilSorted = ownSort(testArrayGanjil)
+// // [ 3, 21, 31, 44, 53, 53, 55, 77, 85, 89 ]
+//
 function ownSort(arr) {
   let hasil = 0;
 
@@ -18,37 +22,36 @@ function ownSort(arr) {
   return arr
 }
 
-function binary_search (search, array) {
-  let awal  = 0;
-  let akhir = array.length;
+// REKURSIF (MASIH GA NGERTI KAK)
 
-  while(awal <= akhir) {
-    let mid = Math.floor((awal + akhir) / 2);
-      if(search === array[mid]) {
-        return mid;
-      } else if(search < array[mid]) {
-        akhir = mid -1;
-      } else if(search > array[mid]) {
-        awal  = mid + 1;
-      }
+function binary_search (search, array, awal = 0, akhir = array.length) {
+  let mid = Math.floor((awal + akhir) / 2);
+
+  if(awal > akhir) {
+    return -1;
+  } else if(search === array[mid]) {
+      return mid;
+  } else if(search < array[mid]) {
+      return binary_search(search, array, awal, akhir = mid - 1);
+  } else if(search > array[mid]) {
+      return binary_search(search, array, awal = mid + 1, akhir);
   }
-  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
-// [ 8 , 10, 10, 18, 22, 22, 32, 40, 90 ]
+// // [ 8 , 10, 10, 18, 22, 22, 32, 40, 90 ]
 var arrayGanjilSorted = ownSort(testArrayGanjil)
-// [ 3, 21, 31, 44, 53, 53, 55, 77, 85, 89 ]
-
-// Driver code
+// // [ 3, 21, 31, 44, 53, 53, 55, 77, 85, 89 ]
+//
+// // Driver code
 console.log(binary_search(8, arrayGenapSorted))
 console.log(binary_search(10, arrayGenapSorted))
 console.log(binary_search(33, arrayGenapSorted))
-
+//
 console.log(binary_search(53, arrayGanjilSorted))
 console.log(binary_search(3, arrayGanjilSorted))
 console.log(binary_search(2, arrayGanjilSorted))
-//
+// //
 module.exports = {
   binary_search
 }
