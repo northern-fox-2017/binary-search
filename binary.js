c2698c93435f3e53045cb8bfd239e5d2be0936d3
Haniiful Wahib <wahib@Haniifuls-MacBook-Pro.local>
2017-11-14 17:19:32 +0700

    finish PSEUDOCODE and ownSort, NOT A RELEASE 0 yet

--- binary.js
+++ binary.js
@@ -5,6 +5,15 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55, 44]
 
 function ownSort(arr) {
   // Your sorting code
+  for (let i = 0 ; i < arr.length; i++){
+    let j = i;
+    while ((arr[j] < arr[j-1]) && (j >= 0) ){
+      let swapTemp = arr[j-1];
+      arr[j-1] = arr[j];
+      arr[j] = swapTemp;
+      j--
+    }
+  }
   return arr
 }
 
@@ -16,15 +25,19 @@ function binarySearch (search, array) {
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
-// Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
 
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
+
+// Driver code
+//onsole.log(arrayGenapSorted);
+//console.log(arrayGanjilSorted);
+console.log(binarySearch(8, arrayGenapSorted))
+console.log(binarySearch(10, arrayGenapSorted))
+console.log(binarySearch(33, arrayGenapSorted))
+
+console.log(binarySearch(53, arrayGanjilSorted))
+console.log(binarySearch(3, arrayGanjilSorted))
+console.log(binarySearch(2, arrayGanjilSorted))
 
 module.exports = {
-  binary_search
+  binarySearch
 }
