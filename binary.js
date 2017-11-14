90e97af8adae04f0129a15e9109e5fbe93dcec04
yulianhp <kono.utsukushi24@gmail.com>
2017-11-14 16:23:01 +0700

    binary search release 0 done

--- binary.js
+++ binary.js
@@ -4,11 +4,22 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // Your sorting code
+  let arrSorted = []
+  while(arr.length > 0){
+    let smallestNum = arr[0]
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i] < smallestNum){
+        smallestNum = arr[i]
+      }
+    }
+    arrSorted.push(smallestNum)
+    arr.splice(arr.indexOf(smallestNum),1)
+  }
+  arr = arrSorted
   return arr
 }
 
-function binarySearch (search, array) {
+function binary_search (search, array) {
   // Your searching code
   return 0;
 }
