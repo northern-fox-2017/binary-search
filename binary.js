eb571b4648f3b1396b50a0b71c8971672ffc43aa
Fajar Patappari <top.sick.red@gmail.com>
2017-11-14 18:25:30 +0700

    Release 0 and 1

--- binary.js
+++ binary.js
@@ -3,24 +3,48 @@
 var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55, 44]
 
-function ownSort(arr) {
-  // Your sorting code
-  return arr
+
+function ownSort(arr) { 
+  let result = []
+  let i = 1
+  while (arr.length > 0) {
+
+    const index = arr.indexOf(i)
+    if (index !== -1) {
+      result.push(i)
+      arr.splice(index, 1)
+    } else {
+      i++
+    }
+  }
+  return result
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function binary_search(search, array) {
+  var minIndex = 0
+  var maxIndex = array.length - 1
+
+  while (minIndex <= maxIndex) {
+    var midIndex = Math.floor((minIndex + maxIndex) / 2)
+    if (array[midIndex] === search) {
+      return midIndex
+    }
+    if (search < array[midIndex]) {
+      maxIndex = midIndex - 1
+    } else {
+      minIndex = midIndex + 1
+    }
+  }
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
-
 // Driver code
 console.log(binary_search(8, arrayGenapSorted))
+
 console.log(binary_search(10, arrayGenapSorted))
 console.log(binary_search(33, arrayGenapSorted))
-
 console.log(binary_search(53, arrayGanjilSorted))
 console.log(binary_search(3, arrayGanjilSorted))
 console.log(binary_search(2, arrayGanjilSorted))
