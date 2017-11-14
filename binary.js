2e6d7f5d75b3fb36e57a2a746a31d942e51c54b2
hacktiv8 <hacktiv8@Sabtas-Mac-mini.local>
2017-11-14 20:40:58 +0700

    done

--- binary.js
+++ binary.js
@@ -4,25 +4,45 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55, 44]
 
 function ownSort(arr) {
-  // Your sorting code
-  return arr
+  for (var i = 1; i < arr.length; i++) {
+     for (var j = 0; j < i; j++) {
+       if(arr[i] < arr[j]){
+         var bantuPindah = arr[i];
+         arr[i] = arr[j];
+         arr[j] = bantuPindah;
+       }
+     }
+  }
+ return arr;
 }
+console.log(ownSort(testArrayGanjil));
+console.log(ownSort(testArrayGenap));
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function binary_search (search, array) {
+  var startIndex = 0;
+  var endIndex = array.length-1;
+  while(startIndex <= endIndex){
+    var midIndex = Math.floor((startIndex+endIndex)/2);
+    if(search > array[midIndex]){
+      startIndex = midIndex+1;
+    } else if(search < array[midIndex]){
+      endIndex = midIndex-1;
+    } else if(search == array[midIndex]){
+      return midIndex;
+    }
+  }
+  return -1
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
 // Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
-
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
+console.log(binary_search(8, arrayGenapSorted)) // 0
+console.log(binary_search(10, arrayGenapSorted)) // 1
+console.log(binary_search(33, arrayGenapSorted)) // -1
+console.log(binary_search(53, arrayGanjilSorted)) // 4
+console.log(binary_search(3, arrayGanjilSorted)) // 0
 console.log(binary_search(2, arrayGanjilSorted))
 
 module.exports = {
